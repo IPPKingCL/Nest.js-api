5b46ed627a3d14737d3a914151d57f5fa6b62777
chobkyu <qudqud97@naver.com>
2022-10-21 01:25:03 +0900

    구조 이해

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
+@Module({  //데코레이터는 클래스에 함수 기능을 추가할 수 있음
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], //컨트롤러는 express의 라우터 같은 존재 url을 가져오고 함수를 실행함
   providers: [AppService],
 })
 export class AppModule {}
--- nest-app/src/app.service.ts
+++ nest-app/src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello fucking World!';
+  }
+
+  getHi():string {
+    return 'hello';
   }
 }
