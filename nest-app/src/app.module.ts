6ffe93f2eaf7b3f2bf3c3de66d88896063fe297a
chobkyu <qudqud97@naver.com>
2022-10-28 00:56:46 +0900

    module

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 
 @Module({  //데코레이터는 클래스에 함수 기능을 추가할 수 있음
-  imports: [],
-  controllers: [MoviesController], //컨트롤러는 express의 라우터 같은 존재 url을 가져오고 함수를 실행함
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], //컨트롤러는 express의 라우터 같은 존재 url을 가져오고 함수를 실행함
+  providers: [],
 })
 export class AppModule {}
