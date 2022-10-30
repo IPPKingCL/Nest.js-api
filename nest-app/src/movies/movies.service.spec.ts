7beff3bb535cc5ac319ea76edaac4f141f1725ee
chobkyu <qudqud97@naver.com>
2022-10-30 23:24:39 +0900

    testing

--- nest-app/src/movies/movies.service.spec.ts
+++ nest-app/src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundError } from 'rxjs';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -15,4 +17,40 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it("should be 4", () => {
+    expect(2+2).toEqual(4);
+  })
+
+  describe("getAll", () => {
+    it("should return an array",()=>{
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+
+    });
+  });
+
+  describe("getOne",() =>{
+    it("should return a movie",() => {
+      service.create({
+        title:"Text Movie",
+        genres : ['test'],
+        year:2000,
+      })
+      const movie = service.getOne(1)
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    
+    it("should throw 404 error",() => {
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Movie with ID : 999`);
+      }
+    })
+  })
+
 });
