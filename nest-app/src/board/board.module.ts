4027be844a8ebfdc91a14b05548587cf4eb40db3
chobkyu <qudqud97@naver.com>
2022-11-16 15:22:17 +0900

    게시판 관련 엔티티 데코레이터 파일 추가

--- nest-app/src/board/board.module.ts
+++ nest-app/src/board/board.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
-@Module({})
-export class BoardModule {
-    /*controllers :[BoardController],
-    providers : [BoardService]*/
-}
+import { BoardRepository } from './repository/board.repository';
+import { TypeOrmExModule } from './repository/typeorm-ex.module';
+@Module({
+    imports:[TypeOrmExModule.forCustomRepository([BoardRepository])],
+    controllers :[BoardController],
+    providers : [BoardService]
+})
+export class BoardModule {}
---
+++ nest-app/src/board/repository/board.repository.ts
@@ -0,0 +1,6 @@
+import { Repository } from "typeorm";
+import { CustomRepository } from "./typeorm-ex.decorator";
+import { BoardEntity } from "../entities/board.entity";
+
+@CustomRepository(BoardEntity)
+export class BoardRepository extends Repository<BoardEntity> {}
