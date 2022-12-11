c17a0d780f3a595e0bcae114f59bd8d2e7dcbe85
chobkyu <qudqud97@naver.com>
2022-12-11 17:12:51 +0900

    api 설명 추가

--- nest-app/src/alcohol/alcohol.controller.ts
+++ nest-app/src/alcohol/alcohol.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AlcoholService } from './alcohol.service';
 
 @Controller('alcohol')
@@ -6,16 +7,19 @@ export class AlcoholController {
     
     constructor(private readonly alchoService : AlcoholService){}
     
+    @ApiOperation({summary:' 술 정보 리스트 조회'})
     @Get('/')
     async getTest(){
         return await this.alchoService.getAll();
     }
 
+    @ApiOperation({summary:' 술 정보 디테일 조회'})
     @Get('/detail/:id')
     async getDetail(@Param('id') id:number){
         return await this.alchoService.getOne(id);
     }
 
+    @ApiOperation({summary:' 술 정보 카테고리 별 조회'})
     @Get('/category/:category')
     async getCategory(@Param('category') category:string){
         return await this.alchoService.getCategory(category);
