4369d921499789d7f581ef57bce20da5ff95f562
chobkyu <qudqud97@naver.com>
2022-12-26 14:56:44 +0900

    주석처리

--- nest-app/src/entities/cocktail.entity.ts
+++ nest-app/src/entities/cocktail.entity.ts
@@ -21,6 +21,9 @@ export class CocktailEntity{
     @Column()
     likeOne : number;
 
+    @Column()
+    only : boolean; //꼭 이 술을 써야하는지 같은 종류의 다른 술을 써도 되는지
+
     @OneToMany((type)=> AlchoRecipeEntity,(alchoRecipeEntity)=>alchoRecipeEntity.cocktail)
     alchoRecipeEntitys : AlchoRecipeEntity[];
 
--- nest-app/src/entities/juice.entity.ts
+++ nest-app/src/entities/juice.entity.ts
@@ -10,7 +10,7 @@ export class JuiceEntity{
     name : string;
 
     @Column()
-    type : number;
+    type : number;  //1.탄산음료 2.칵테일용 탄산음료 3.즙 4.과일주스
 
     @Column({length:500})
     imgUrl : string;
