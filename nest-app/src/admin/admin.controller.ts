5cfa49511c2da6ab27a06eb441e9139c04ab0c20
chobkyu <qudqud97@naver.com>
2023-01-25 16:29:05 +0900

    Update admin.controller.ts

--- nest-app/src/admin/admin.controller.ts
+++ nest-app/src/admin/admin.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Logger ,Headers, Body, Post } from '@nestjs/common';
+import { Controller, Get, Logger ,Headers, Body, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/user/jwt/jwt.guard';
 import { getToken } from 'src/util/token';
 import { AdminService } from './admin.service';
 import { InsertCocktailDto } from './dto/insertCocktail.Dto';
@@ -10,6 +11,7 @@ export class AdminController {
     constructor(private readonly adminService : AdminService){};
 
     @ApiOperation({summary: '새로운 칵테일 페이지 추가 시 필요 작업'})
+    @UseGuards(JwtAuthGuard)
     @Get('/newCocktail')
     async newCocktail(@Headers() header){
         return await this.adminService.newCocktail(getToken(header));
