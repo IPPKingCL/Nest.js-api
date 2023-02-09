d92ecee0503d9f09631f4674d0cc03cb33916306
chobkyu <qudqud97@naver.com>
2023-02-09 10:39:41 +0900

    insert self cock
    테스트는 아직 안해봄

--- nest-app/src/selfcocktail/selfcocktail.service.ts
+++ nest-app/src/selfcocktail/selfcocktail.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InsertCocktailDto } from 'src/admin/dto/insertCocktail.Dto';
+import { CocktailEntity } from 'src/entities/cocktail.entity';
+import { JuiceRecipeEntity } from 'src/entities/juiceRecipe.entity';
 import { DataSource } from 'typeorm';
 import { SelfAlchoRecipeRepository } from './repository/selfAlchoRecipe.repository';
 import { SelfCocktailRepository } from './repository/selfCocktail.repository';
@@ -26,7 +28,7 @@ export class SelfcocktailService {
             const token = this.jwtService.decode(header);
 
             const res = await this.insertSelfCocktail(insertDto);
-
+         
             if(res.success){
                 return {success:true};
             }else{
@@ -39,12 +41,42 @@ export class SelfcocktailService {
     }
 
     async insertSelfCocktail(insertDto:InsertCocktailDto){
+        const queryRunner = this.dataSource.createQueryRunner(); //queryRunner 생성
+        await queryRunner.connect();  //queryRunner 연결
+        await queryRunner.startTransaction(); //트랜잭션 시작
+
         try{
+            const cocktail = new CocktailEntity();
+            cocktail.name = insertDto.name;
+            cocktail.imgUrl = insertDto.imgUrl;
+            cocktail.dosu = insertDto.dosu;
+
+            const res = await queryRunner.query(
+                "insert into selfCocktail(name, dosu, imgUrl) values ('"+cocktail.name+"',"+cocktail.dosu+",'"+cocktail.imgUrl+"')"
+            );
+
+            console.log(res['insertId']);
+            const id = res['insertId'];
+
+            const resAlcho = await this.insertSelfAlchoRecipe(id,insertDto.alcho, queryRunner);
+            const resJuice = await this.insertSelfJuiceRecipe(id,insertDto.juice,queryRunner);
+
+            if(resAlcho.success && resJuice.success){
+                console.log("트랜잭션 커밋")
+                await queryRunner.commitTransaction();
+                return {success:true};
+            }else{
+                console.log("트랜잭션 롤백")
+                await queryRunner.rollbackTransaction();
+                return {success:false , msg:"칵테일 삽입 중 에러 발생"};
+            }
 
         }catch(err){
             this.logger.error(err);
             this.logger.log('transaction rollback!!');
             return {success:false, msg:'칵테일 삽입 중 에러 발생'};
+        }finally{
+            await queryRunner.release();
         }
     }
 
@@ -55,6 +87,33 @@ export class SelfcocktailService {
                 +'values ('+insertDto[0].amount+','+insertDto[0].only+','+id+','+insertDto[0].unit+','+insertDto[0].name+')'
             );
 
+            return {success:true};
+        }catch(err){
+            this.logger.error(err);
+            return {success:false,msg:err};
+        }
+    }
+
+    async insertSelfJuiceRecipe(id,insertDto,queryRunner){
+        try{
+            const juiceRecipe = new JuiceRecipeEntity();
+
+            console.log(insertDto[0]);
+            console.log(insertDto[0].id);
+
+            juiceRecipe.cocktail = id;
+            juiceRecipe.amount = insertDto.amount;
+            juiceRecipe.juice = insertDto.id;
+            juiceRecipe.unitNum = insertDto.unit;
+            
+            for(let i = 0; i<insertDto.length;i++){
+                await queryRunner.query(
+                    'insert into selfJuiceRecipe(amount, juiceId, cocktailId, unitNumId) '
+                    +'values ('+insertDto[i].amount+','+insertDto[i].name+','+id+','+insertDto[i].unit+')'
+                );
+            }
+            
+            
             return {success:true};
         }catch(err){
             this.logger.error(err);
