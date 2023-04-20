dd8076febf1f141cd8450d6851dc5bc8f9215cc5
chobkyu <qudqud97@naver.com>
2023-04-20 23:52:03 +0900

    FaceChat setting

--- nest-app/src/face-chat/face-chat.controller.ts
+++ nest-app/src/face-chat/face-chat.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { FaceChatService } from './face-chat.service';
 
 @Controller('face-chat')
-export class FaceChatController {}
+export class FaceChatController {
+    private readonly logger = new Logger(FaceChatController.name)
+    constructor(
+        private readonly faceChatService : FaceChatService,
+    ){}
+}
--- nest-app/src/face-chat/face-chat.service.ts
+++ nest-app/src/face-chat/face-chat.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { FaceChatRepository } from './repository/faceChat.repository';
+import { FaceChatMemRepository } from './repository/faceChatMem.repository';
 
 @Injectable()
-export class FaceChatService {}
+export class FaceChatService {
+    private readonly logger = new Logger(FaceChatService.name);
+    constructor(
+        private readonly faceChatRepository : FaceChatRepository,
+        private readonly faceChatMemRepository : FaceChatMemRepository
+    ){}
+}
