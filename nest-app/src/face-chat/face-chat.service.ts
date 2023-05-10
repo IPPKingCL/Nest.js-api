c6e767ae81cbd141577b7024e75428a09af21da5
chobkyu <qudqud97@naver.com>
2023-05-10 23:31:15 +0900

    Update face-chat.service.ts

--- nest-app/src/face-chat/face-chat.service.ts
+++ nest-app/src/face-chat/face-chat.service.ts
@@ -2,13 +2,15 @@ import { Injectable, Logger } from '@nestjs/common';
 import { FaceChatRepository } from './repository/faceChat.repository';
 import { FaceChatMemRepository } from './repository/faceChatMem.repository';
 import { FaceChatEntity } from 'src/entities/faceChat.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class FaceChatService {
     private readonly logger = new Logger(FaceChatService.name);
     constructor(
         private readonly faceChatRepository : FaceChatRepository,
-        private readonly faceChatMemRepository : FaceChatMemRepository
+        private readonly faceChatMemRepository : FaceChatMemRepository,
+        private jwtService : JwtService
     ){}
 
     async getAllFaceChat() : Promise<FaceChatEntity[]|object>{
@@ -32,7 +34,20 @@ export class FaceChatService {
     
     async addFaceChat(addFaceChatDto, header){
         try{
+            const token = this.jwtService.decode(header);
 
+            const faceChatEntity = new FaceChatEntity();
+
+            faceChatEntity.roomName = addFaceChatDto.name;
+            faceChatEntity.detailComment = addFaceChatDto.detailComment;
+            faceChatEntity.alchoCategory = addFaceChatDto.category;
+            faceChatEntity.user = token["id"];
+            faceChatEntity.dateTime = new Date();
+            
+            await this.faceChatRepository.save(faceChatEntity);
+
+            return {success:true};
+            
         }catch(err){
             this.logger.error(err);
             return {success:false};
