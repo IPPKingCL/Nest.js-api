de3437b66de85b9b09fa1e4e4d9cba51af910b5f
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-05-15 16:54:10 +0900

    add ddos detection

---
+++ nest-app/src/middleware/limiter.modules.ts
@@ -0,0 +1,21 @@
+const limit = require('express-rate-limit');
+
+exports.limiter = new limit({
+	windowMs: 60000,
+	max: 5,
+	delayMs: 1000,
+	handler(req, res) {
+		res.status(this.statusCode).json({
+			code: this.statusCode,
+			message: '1분에 5번 1초씩 요청가능'
+		});
+	}
+});
+
+//express 사용 예시
+/*
+const { limiter } = require('./limit');
+
+router.get('/test', limiter, (req, res) => {
+	res.send("aaa");
+});*/
